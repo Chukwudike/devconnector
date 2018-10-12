12098830e68ba82c06242f39328880035cc252e3
chukwudike <bryan.uwanaka@gmail.com>
2018-10-12 01:16:13 +0100

    still on the appropriate keys

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,7 @@
 if(process.env.NODE_ENV==="production"){
-  module.exports = require('./keys.dev')
-}else {
   module.exports = require('./keys.prod')
+}else {
+  module.exports = require('./keys.dev')
 }
 
-// module.exports = {
-//   Mongourl: "mongodb://dike:dike99@ds139632.mlab.com:39632/dikconnector",
-//   secretOrKey: 'secret'
-// };
+
