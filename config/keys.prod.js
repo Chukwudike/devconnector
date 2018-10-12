dac34d95ffea7839d59cfb5a7aa8514f07c79e63
chukwudike <bryan.uwanaka@gmail.com>
2018-10-12 01:02:34 +0100

    I used the appropriate keys for production

--- config/keys.prod.js
+++ config/keys.prod.js
@@ -1,6 +1,5 @@
 module.exports = {
-   Mongourl : "mongodb://localhost:27017/devvapp",  
-  // Mongourl: "mongodb://dike:dike99@ds139632.mlab.com:39632/dikconnector",
-    secretOrKey: 'secret'
+    Mongourl: process.env.MONGO_URL,
+    secretOrKey: process.env.SECRET_OR_KEY
   };
   
